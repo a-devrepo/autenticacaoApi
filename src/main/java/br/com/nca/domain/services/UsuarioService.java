e8c0b8a757880291f70d62e473527d5cb8965052
alison <adevpr11@gmail.com>
2025-09-10 12:19:25 -0300

    feat: adiciona em UsuarioService dependência do JWTHelper e implementa lógica de atribuição do token

--- src/main/java/br/com/nca/domain/services/UsuarioService.java
+++ src/main/java/br/com/nca/domain/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.nca.domain.services;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.springframework.stereotype.Service;
 
@@ -9,20 +10,25 @@ import br.com.nca.domain.dtos.AutenticarUsuarioResponseDTO;
 import br.com.nca.domain.dtos.CriarUsuarioRequestDTO;
 import br.com.nca.domain.dtos.CriarUsuarioResponseDTO;
 import br.com.nca.domain.entities.Usuario;
-import br.com.nca.domain.exceptions.DuplicateEmailException;
 import br.com.nca.domain.exceptions.AccessDeniedException;
+import br.com.nca.domain.exceptions.DuplicateEmailException;
 import br.com.nca.domain.repositories.UsuarioRepository;
 import br.com.nca.helpers.CryptoHelper;
+import br.com.nca.helpers.JWTHelper;
 
 @Service
 public class UsuarioService {
 
+    private final JWTHelper jwtHelper;
+
     private UsuarioRepository repository;
     private CryptoHelper cryptoHelper;
     
-    public UsuarioService(UsuarioRepository repository, CryptoHelper cryptoHelper) {
+    public UsuarioService(UsuarioRepository repository, 
+            CryptoHelper cryptoHelper, JWTHelper jwtHelper) {
         this.repository = repository;
         this.cryptoHelper = cryptoHelper;
+        this.jwtHelper = jwtHelper;
     }
     
     public CriarUsuarioResponseDTO criarUsuario(CriarUsuarioRequestDTO requestDto) {
@@ -49,7 +55,10 @@ public class UsuarioService {
             throw new AccessDeniedException();
         }
         
-        return entityToAntenticateResponse(user);
+        var token = jwtHelper.getToken(user.getId());
+        
+        
+        return entityToAntenticateResponse(user, token);
     }
     
     private Usuario requestToEntity(CriarUsuarioRequestDTO dto) {
@@ -70,16 +79,21 @@ public class UsuarioService {
                 .build();
     }
     
-    private AutenticarUsuarioResponseDTO entityToAntenticateResponse(Usuario usuario) {
+    private AutenticarUsuarioResponseDTO entityToAntenticateResponse(Usuario usuario, String token) {
         
-        var dataHoraAcesso = LocalDateTime.now();
+        var dataHoraAcesso = jwtHelper.getCurrentDate()
+                .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        
+        var dataHoraExpiracao = jwtHelper.getExpiration()
+                .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         
         return AutenticarUsuarioResponseDTO.builder()
                 .id(usuario.getId())
                 .nome(usuario.getNome())
                 .email(usuario.getEmail())
                 .dataHoraAcesso(dataHoraAcesso)
-                .dataHoraExpiracao(dataHoraAcesso.plusHours(1))
+                .dataHoraExpiracao(dataHoraExpiracao)
+                .token(jwtHelper.getToken(usuario.getId()))
                 .build();
     }
 }
